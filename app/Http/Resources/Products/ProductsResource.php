5030259875b92aa4b702ca76bdb3620e2612da32
matin tayebi <mattttin123.007@gmail.com>
2022-11-16 08:25:31 +0330

    added products resource for foods api

---
+++ app/Http/Resources/Products/ProductsResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Products;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|\JsonSerializable
+     */
+    public function toArray($request): array|\JsonSerializable|Arrayable
+    {
+        return parent::toArray($request);
+    }
+}
