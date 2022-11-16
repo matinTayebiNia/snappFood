90d23b1ba11ce03e6ac558ee32fad9f4ce62fc59
matin tayebi <mattttin123.007@gmail.com>
2022-11-16 08:24:06 +0330

    added api resource for foods api

--- app/Http/Controllers/Apis/ApiV1/ProductController.php
+++ app/Http/Controllers/Apis/ApiV1/ProductController.php
@@ -3,23 +3,18 @@
 namespace App\Http\Controllers\Apis\ApiV1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Products\ProductsResource;
 use App\Models\Category;
-use App\Models\Place;
 use App\Models\Product;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use PHPUnit\Exception;
 
 class ProductController extends Controller
 {
     /**
      * @param $place
-     * @return JsonResponse
+     * @return ProductsResource|JsonResponse
      */
-    public function foods($place): JsonResponse
+    public function foods($place): JsonResponse|ProductsResource
     {
         try {
             $foods = Category::where("type", "ForFood")
@@ -30,27 +25,8 @@ class ProductController extends Controller
                     return $query->where("place_id", $place);
                 })->get();
 
-            return $this->successMessage(["categories" => $foods]);
+            return new ProductsResource($foods);
 
-        } catch (\Exception $exception) {
-            return $this->throwErrorMessageException([
-                "message" => $exception->getMessage(),
-                "code" => $exception->getCode()
-            ]);
-        }
-    }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param Product $product
-     * @return JsonResponse
-     */
-    public function show(Product $product): JsonResponse
-    {
-        try {
-            return $this->successMessage(["data" => $product->toJson()]);
         } catch (\Exception $exception) {
             return $this->throwErrorMessageException([
                 "message" => $exception->getMessage(),
